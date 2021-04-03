a409762e9cfbd84e6f517aeb39b7de32cf0b6383
jasonclark <jasclark@gmail.com>
2021-04-02 20:57:34 -0600

    corrected instruction to use create-json.py

--- assets/json/create-json.py
+++ assets/json/create-json.py
@@ -3,7 +3,7 @@
 # navigate to images directory, use tree command to generate list of files
 # tree > ../json/tree.txt
 # navigate to json directory, run python script to generate json file of images
-# python main.py
+# python create-json.py
 
 import json
 text = open('tree.txt', 'rb')
