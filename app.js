7fe667ec319f5e456622390497ce88d9af2e2296
jasonclark <jasclark@gmail.com>
2021-04-03 13:21:21 -0600

    set output for unfiltered results, set display for new HTML tags

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let net;
+let output;
 const robot = '🤖';
 
 async function app() {
@@ -9,34 +10,43 @@ async function app() {
   net = await mobilenet.load();
   console.log('Successfully loaded model');
 
-    // Make a prediction through the model on our image.
-    const img = document.getElementById('source');
-    const result = await net.classify(img);
-    console.log(result);
+  // Make a prediction through the model on our image.
+  const img = document.getElementById('source');
+  const result = await net.classify(img);
+  console.log(result);
+  
+  // Loop through and print complete prediction array results
+  result.forEach((item, index) => {
+    //console.log(`index: ${index}, label: ${item.className}, confidence: ${item.probability}`);
+    output = `
+      <li>index: ${index}, label: ${item.className}, confidence: ${item.probability}</li>
+    `;
+    document.getElementById('unfiltered').innerHTML += `${output}`;
+  });
     
-    // Sort by probability
-    let resultElements = result.sort((a, b) => a > b)[0];
+  // Sort by probability
+  let resultElements = result.sort((a, b) => a > b)[0];
 
-    if (resultElements.probability > 0.2) {
-      // Convert the probability to percentages
-      const probabilityPercent = Math.round(resultElements.probability * 100);
-      // Display result
-      document.getElementById('console').innerText = `
-        ${robot} ${probabilityPercent}% certain this is a ${resultElements.className.replace(","," or")}.
-      `;
-    } else { 
-      document.getElementById('console').innerText = `
-        ${robot} I am not sure what I should recognize and my prediction probability is low.
-      `;
-    }
-
+  if (resultElements.probability > 0.2) {
     // Convert the probability to percentages
-    //const probabilityPercent = Math.round(result.probability * 100);
-    // Print result to target location on the web page
-    //document.getElementById('console').innerText = `
-      //prediction: ${result[0].className}\n
-      //probability: ${result[0].probability} (which is about ${probabilityPercent}% confidence in the prediction)
-    //`;
+    let probabilityPercent = Math.round(resultElements.probability * 100);
+    // Display result
+    document.getElementById('console').innerText = `
+      ${robot} ${probabilityPercent}% certain this is a ${resultElements.className.replace(","," or")}.
+    `;
+  } else { 
+    document.getElementById('console').innerText = `
+      ${robot} I am not sure what I should recognize and my prediction probability is low.
+    `;
+  }
+
+  // Convert the probability to percentages
+  //const probabilityPercent = Math.round(result.probability * 100);
+  // Print result to target location on the web page
+  //document.getElementById('console').innerText = `
+    //prediction: ${result[0].className}\n
+    //probability: ${result[0].probability} (which is about ${probabilityPercent}% confidence in the prediction)
+  //`;
 }
 
 app();
