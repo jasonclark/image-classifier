5d0fb4f7c8a24f99d8231987a302bff51ff1fa8f
jasonclark <jasclark@gmail.com>
2021-04-04 17:49:49 -0600

    added onload event to randomly load img and then run renamed classify function

--- app.js
+++ app.js
@@ -2,7 +2,31 @@ let net;
 let output;
 const robot = '🤖';
 
-async function app() {
+window.onload = () => {
+  // Load random image from data.json for prediction analysis
+  fetch('./assets/json/data.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(imgList => { 
+    //console.log(imgList);
+    let imgSrc = imgList.images[Math.floor(Math.random() * imgList.images.length)];
+    //console.log(imgSrc);
+    const img = document.getElementById('source');
+    // Set img attributes
+    img.setAttribute("src", `./assets/img/${imgSrc}`);
+    img.setAttribute("height", '264');
+    img.setAttribute("width", '378');
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  });
+};
+
+async function classify() {
   console.log('Loading mobilenet..');
   document.getElementById('console').innerText = `Loading the model and getting a prediction...`;  
 
@@ -27,7 +51,7 @@ async function app() {
   // Sort by probability
   let resultElements = result.sort((a, b) => a > b)[0];
 
-  if (resultElements.probability > 0.2) {
+  if (resultElements.probability > 0.15) {
     // Convert the probability to percentages
     let probabilityPercent = Math.round(resultElements.probability * 100);
     // Display result
@@ -49,4 +73,4 @@ async function app() {
   //`;
 }
 
-app();
+classify();
